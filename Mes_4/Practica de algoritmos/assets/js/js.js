4d45bdd517a7199522dcf1bc9202f7d6984d7c67
fenanda_tc <ftorres@estudiantes.crackthecode.la>
2022-01-05 10:15:00 -0500

    subida_1

--- Mes_4/Practica de algoritmos/assets/js/js.js
+++ Mes_4/Practica de algoritmos/assets/js/js.js
@@ -4,8 +4,23 @@
 
 // variables de un partido de voley
 
-let deporte = "Boley";
-let equipos = ["equipo1", "equipo2"];
-let contadorDePenalidades = 0;
-let contadorDePuntos=0;
-let ganador = equipos[0]
+// let deporte = "Boley";
+// let equipos = ["equipo1", "equipo2"];
+// let contadorDePenalidades = 0;
+// let contadorDePuntos=0;
+// let ganador = equipos[0]
+
+$(document).ready(function(){
+    $("btn-ingresar").click(function(){
+        var correo = $("#mail").val();
+        var clave = $("#pass").val();
+        if(correo=="ctc@gmail.com"& clave == "1234"){
+            alert("Datos correctos")
+            // llevar al usuario a la web
+        }else{
+            alert("Datos Incorrectos")
+            // popup de para avisar
+        }
+    })
+})
+
